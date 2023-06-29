7da2e1927ee64971ed22384f21a7db8caeabd9da
Johannes Niedens <johannesniedens@gmail.com>
2023-06-30 00:51:44 +0200

    updated backend

--- backend/app.js
+++ backend/app.js
@@ -1,53 +1,43 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const fetch = require("node-fetch");
 const cors = require("cors");
-const fs = require("fs").promises;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/shortener", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const Link = mongoose.model("Link", {
-    name: String,
-    url: String,
-});
+let links = [];  // Using array to store links
+let idCounter = 0;  // Counter to generate unique ids
 
 app.post("/links", async (req, res) => {
     const { name, url } = req.body;
 
-    // For demonstration purposes we are not validating the url
-    const link = new Link({ name, url });
-    await link.save();
+    try {
+        await fetch(url, { method: "HEAD" });
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid URL provided." });
+    }
+
+    const link = { id: idCounter++, name, url };
+    links.push(link);
 
     res.json({ message: "Link created!", link });
 });
 
 app.get("/links", async (req, res) => {
-    const links = await Link.find();
     res.json(links);
 });
 
 app.get("/links/:id/content", async (req, res) => {
-    const link = await Link.findById(req.params.id);
+    const link = links.find(link => link.id == req.params.id);
 
     if (!link) {
         return res.status(404).json({ message: "Link not found." });
     }
 
-    let content;
     try {
-        if (link.url.startsWith("file://")) {
-            content = await fs.readFile(link.url.substring(7), "utf8");
-        } else {
-            const response = await fetch(link.url);
-            content = await response.text();
-        }
+        const response = await fetch(link.url);
+        const content = await response.text();
 
         res.json({ content });
     } catch (error) {
@@ -56,15 +46,37 @@ app.get("/links/:id/content", async (req, res) => {
 });
 
 app.delete("/links/:id", async (req, res) => {
-    const link = await Link.findById(req.params.id);
+    const index = links.findIndex(link => link.id == req.params.id);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "Link not found." });
+    }
+
+    links.splice(index, 1);
+
+    res.json({ message: "Link deleted!" });
+});
+
+app.get("/links/:id/download", async (req, res) => {
+    const link = links.find(link => link.id == req.params.id);
 
     if (!link) {
         return res.status(404).json({ message: "Link not found." });
     }
 
-    await link.deleteOne();
+    try {
+        const response = await fetch(link.url);
+        const content = await response.text();
 
-    res.json({ message: "Link deleted!" });
+        res.setHeader(
+            "Content-Disposition",
+            `attachment; filename=${link.name}.html`
+        );
+        res.setHeader("Content-Type", "text/html");
+        res.send(content);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch link content." });
+    }
 });
 
 app.listen(5000, () => console.log("Server started on port 5000"));
